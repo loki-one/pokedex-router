f3faea7257315b1736fe1ec534a6813c16b1eaed
loki-one <lokesh.jadhav10@gmail.com>
2018-02-21 16:48:40 +0100

    :fire: remove predefined styles, add logos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img id="logo" src="./assets/pokemon-logo.png">
     <router-view/>
   </div>
 </template>
@@ -19,5 +19,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#logo {
+  height: 300px;
 }
 </style>
